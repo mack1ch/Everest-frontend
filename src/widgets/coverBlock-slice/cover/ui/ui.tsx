481aa4ac9c2cd6fcf3b73f1fd737f968f6e25acf
Dmitriy <dimastepanov.1995@mail.ru>
2024-02-10 19:59:48 +0500

    added: newassets, changed: background to imgs

--- src/widgets/coverBlock-slice/cover/ui/ui.tsx
+++ src/widgets/coverBlock-slice/cover/ui/ui.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import { ButtonCover } from "@/entities/buttons-slice/buttonCover";
 import styles from "./ui.module.scss";
-
+import Image from "next/image";
+import Cover from "../../../../../public/assets/coverIMG.png";
+import CoverSVG from "../../../../../public/assets/coverIMG.svg";
+import { useWindowSize } from "@/shared/hooks/useWindowSize";
 export const CoverBlock = () => {
+  const windowSize = useWindowSize();
   return (
     <>
       <div className={styles.coverBlock}>
@@ -17,6 +23,15 @@ export const CoverBlock = () => {
             Отправить заявку
           </ButtonCover>
         </section>
+        <Image
+          className={styles.img}
+          style={{ marginTop: "80px", userSelect: "none" }}
+          src={CoverSVG}
+          blurDataURL={Cover.src}
+          width={windowSize < 800 ? 279 : windowSize > 1200 ? 542 : 387}
+          height={windowSize < 800 ? 195 : windowSize > 1200 ? 379 : 271}
+          alt="Cover"
+        />
       </div>
     </>
   );
